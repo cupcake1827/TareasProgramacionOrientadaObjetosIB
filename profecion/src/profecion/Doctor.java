a164f1f2f97b427995a4f6c2348712b30545315c
cupcake1827 <62129644+cupcake1827@users.noreply.github.com>
2020-03-21 01:14:55 -0600

    init commit

---
+++ profecion/src/profecion/Doctor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profecion;
+import Persona.persona;
+/**
+ *
+ * @author Jimmy Ramirez
+ */
+
+public class Doctor extends persona {
+    
+
+    public Doctor() {
+        
+        setNombre("Isis Sauceda");
+        setEdad(28);
+        setTrabajo("Hospital del Caribe");
+    
+}
+    
+}
